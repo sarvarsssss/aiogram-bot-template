f4d0e9063dca428ee10effaeb387501ec4f51182
sarvarsssss <88589827+sarvarsssss@users.noreply.github.com>
2022-01-17 14:18:14 +0500

    Update info.py

--- handlers/users/info.py
+++ handlers/users/info.py
@@ -18,42 +18,42 @@ async def show_menu(message: Message):
 @dp.message_handler(text='Izzat')
 async def send_photo(message: Message):
     photo_id = "AgACAgIAAxkBAANNYSzVlicoSGeWFTJ2zU9s3N3TdlcAAi-1MRs8yGlJ4egtyjY2G7sBAAMCAAN5AAMgBA"
-    await message.reply_photo(photo_id,caption="Pidaraz bola \nOdam bo'lishi juda qiyin Mansabparaz \nLaqabi: 'Xamelion'", reply_markup=menufriends)
+    await message.reply_photo(photo_id,caption="Pidaraz bola \nOdam bo'lishi juda qiyin Mansabparaz \nLaqabi: 'Xamelion' \nShiori: 'O'zinio'yla o'zgaga tupur", reply_markup=menufriends)
 
 @dp.message_handler(text='Islom')
 async def send_photo(message: Message):
     photo_id = "AgACAgIAAxkBAAM5YSzPD73Dsny62RuCqrbnHyKvCNkAAiO1MRs8yGlJHEGXinX8rkkBAAMCAAN5AAMgBA"
-    await message.reply_photo(photo_id, caption="Samimiy bola Big Bon kampaniyasi Asoschisi \nShou Biznes yulduzi \nO'zining unitilmas qo'shiqlari bilan tomoshabin qalbidan joy olgan", reply_markup=menufriends)
+    await message.reply_photo(photo_id, caption="Samimiy bola Big Bon kampaniyasi Asoschisi \nShou Biznes yulduzi \nO'zining unitilmas qo'shiqlari bilan tomoshabin qalbidan joy olgan \nShiori:Big Bon ko'rdim xamirlari bo'zlab kelar \nShu Bog Bonda mani dodam izlab kelar", reply_markup=menufriends)
 
 @dp.message_handler(text='Xabibullo')
 async def send_link(message: Message):
     photo_id = "AgACAgIAAxkBAANFYSzS3MA9hUntbjJizP60feESU20AAiq1MRs8yGlJ5nCKFlJ4_4cBAAMCAAN5AAMgBA"
-    await message.reply_photo(photo_id,caption="Amazon Asoschisi \nSeryozniy yurishi yoqtiralla \nJuda chuqur O'ylidila Yaqnda shu chuqurga o'zlari tushub ketadila " , reply_markup=menufriends)
+    await message.reply_photo(photo_id,caption="Amazon Asoschisi \nSeryozniy yurishi yoqtiralla \nJuda chuqur O'ylidila Yaqnda shu chuqurga o'zlari tushub ketadila \nShiori: 'Life is good but it can be better'" , reply_markup=menufriends)
 
 @dp.message_handler(text='Jamshid')
 async def send_link(message: Message):
     photo_id = "AgACAgIAAxkBAANLYSzUoNkFEDOUZYBK08QPcY8u_fEAAi21MRs8yGlJNw9RBzYCz08BAAMCAAN5AAMgBA"
-    await message.reply_photo(photo_id,caption="Professor Xayolparas \nYordami ko'p tegadi \nMani ustozim \nLaqabi: 'Krakadil'", reply_markup=menufriends)
+    await message.reply_photo(photo_id,caption="Professor Xayolparas \nYordami ko'p tegadi \nMani ustozim \nLaqabi: 'Krakadil' \nShiori:'Do'stlarga rejalaringizni aytmang Ularga natijalarni ko'rsating'", reply_markup=menufriends)
 
 @dp.message_handler(text='Jamol')
 async def send_link(message: Message):
     photo_id = "AgACAgIAAxkBAANHYSzTmRNq5w_dHjS8b4x9KdAFeHQAAiu1MRs8yGlJdtOezQUnmvABAAMCAAN4AAMgBA"
-    await message.reply_photo(photo_id, caption="Bitcoin Asoschisi Satoshi Nakomoto \nJuda harakatchan bola \nLaqabi: 'Jumong','Tutqich bermas chumoli' ", reply_markup=menufriends)
+    await message.reply_photo(photo_id, caption="Bitcoin Asoschisi Satoshi Nakomoto \nJuda harakatchan bola \nLaqabi: 'Jumong','Tutqich bermas chumoli' \nXozir gruppadan chopilgan Sabablari nomalum \nTez kunda bunga oydinlik kiritamiz yo'ki oydinnikiga kirgizvoramiz ", reply_markup=menufriends)
 
 @dp.message_handler(text='Ilyos')
 async def send_link(message: Message):
     photo_id = "AgACAgIAAxkBAANDYSzSW86chxwcvgaEEeGx26JtSNUAAim1MRs8yGlJRxaTdkfpt1ABAAMCAAN4AAMgBA"
-    await message.reply_photo(photo_id,caption="Butun dunyoni qogoz bilan taminlidi \nBurgani Ko'tini ko'rgan bola \nLaqabi: 'Akula'", reply_markup=menufriends)
+    await message.reply_photo(photo_id,caption="Butun dunyoni qogoz bilan taminlidi \nBurgani Ko'tini ko'rgan bola \nSHiori:'Hammasiga uzoqdan kulib qo'ydik' \nLaqabi: 'Akula'", reply_markup=menufriends)
 
 @dp.message_handler(text='Ibrohim')
 async def send_link(message: Message):
     photo_id = "AgACAgIAAxkBAANPYSzV3KhlzQAB2pTkWpf7JJ_BRai4AAIytTEbPMhpSb5CPuDQbomnAQADAgADeAADIAQ"
-    await message.reply_photo(photo_id,caption="Media Park Asoschisi \nOdil bilan Nurini O'rtogi \nLaqabi: 'Suv mushugi'", reply_markup=menufriends)
+    await message.reply_photo(photo_id,caption="Media Park Asoschisi \nOdil bilan Nurini O'rtogi \nLaqabi: 'Suv mushugi' \nBura o'ynashi bilmidi to'risi vasheee blmidi \Shiori:'Ertalab ishda qarsak chalish sogliq ucun foydali' ", reply_markup=menufriends)
 
 @dp.message_handler(text='Shohjahon')
 async def send_link(message: Message):
     photo_id = "AgACAgIAAxkBAANJYSzUJ_PIH83qgYhbzNV5-Pfg1kkAAiy1MRs8yGlJUqyliAKvw6sBAAMCAAN4AAMgBA"
-    await message.reply_photo(photo_id, caption="Elektra set Direktori \nJuda aqlli aka Yaxshi maslahatla chiqadi \nYaqnda to'y bo'ladi ", reply_markup=menufriends)
+    await message.reply_photo(photo_id, caption="Elektra set Direktori \nJuda aqlli aka Yaxshi maslahatla chiqadi \nYaqnda to'y  bo'lad Shundan so'ng uni ko'rmadik \nShiori: 'uylanish uylanish uylanish'", reply_markup=menufriends)
 
 
 @dp.message_handler(text='Log Out')
